9f7ea3d2eecc59dd6be2ca56baef3085091225a5
aluabagdat <alukabagdat@gmail.com>
2025-01-17 20:34:30 +0500

    upd 5.py

--- lab1/5.py
+++ lab1/5.py
@@ -6,3 +6,32 @@ myVar = "John"
 MYVAR = "John"
 myvar2 = "John"
 
+#example2 multiple variables
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+#example3 unpack a list
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+#example4 function
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+#example5 global keyword
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
