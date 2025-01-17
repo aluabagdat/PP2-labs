327e56cfb5aacaa3a3e7657ca0c08f9f9cf62cc2
aluabagdat <alukabagdat@gmail.com>
2025-01-17 20:16:57 +0500

    upd 4.py

--- lab1/4.py
+++ lab1/4.py
@@ -1,2 +1,26 @@
+#example1
 x = 5
-y = "Hello, World!"
+y = "John"
+print(x)
+print(y)
+
+#example2
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+#example3 get the type
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#example4 quotes
+x = "John"
+# is the same as
+x = 'John'
+
+#example5 case-sensitive
+a = 4
+A = "Sally"
+#A will not overwrite a
