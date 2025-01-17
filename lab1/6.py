33ca00dec30324e1c381835c718b502be5b0de6d
aluabagdat <alukabagdat@gmail.com>
2025-01-17 20:46:04 +0500

    upd 7.py

--- lab1/6.py
+++ lab1/6.py
@@ -1,2 +1,51 @@
-#print("Hello, World!")
-print("Cheers, Mate!")
+#example1 chang from one type to another
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#example2 random
+import random
+
+print(random.randrange(1, 10))
+
+#example3 integers
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+
+#example4 floats
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+#example5 strings
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+
+#example6 Get the character at position 1
+a = "Hello, World!"
+print(a[1])
+
+#example7
+for x in "banana":
+  print(x)
+
--- lab1/7.py
+++ lab1/7.py
@@ -1,4 +1,12 @@
-#This is a comment
-#written in
-#more than just one line
-print("Hello, World!")
+#example1 upper case
+a = "Hello, World!"
+print(a.upper())
+
+#example2 lower case
+b = "Hello, World!"
+print(b.lower())
+
+#example3 replace
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
