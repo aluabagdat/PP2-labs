246cb3285a2a0abb10a5a08074249f57b6ed6d47
aluabagdat <alukabagdat@gmail.com>
2025-01-17 20:13:58 +0500

    upd 3.py

--- lab1/3.py
+++ lab1/3.py
@@ -1,2 +1,17 @@
+#example1
 #print("Hello, World!")
 print("Cheers, Mate!")
+
+#example2
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+
+#example3
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
