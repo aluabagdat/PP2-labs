ad5d936d1437fc4f249ddea41431dac9b788856b
alukabagdat <alukabagdat@gmail.com>
2025-02-02 21:02:24 +0500

    upd 6.py

---
+++ lab3/function1/6.py
@@ -0,0 +1,11 @@
+"""
+Write a function that accepts string from user, 
+return a sentence with the words reversed. 
+"""
+s = str(input())
+def reverse(s):
+    x = s.split
+    x.reverse()
+    return x
+
+print(reversed(str))
