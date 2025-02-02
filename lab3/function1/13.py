654f779340792d3ce5f0407fa3ad3e811b07ee2a
alukabagdat <alukabagdat@gmail.com>
2025-02-02 22:30:22 +0500

    upd 13.py

--- lab3/function1/13.py
+++ lab3/function1/13.py
@@ -2,23 +2,25 @@
 Write a program able to play the "Guess the number" - game,
 where the number to be guessed is randomly chosen between 1 and 20. 
 """
-
 import random
-name = input("Hello! What is your name?\n")
 
-print( f"Well,{name}, I am thinking of a number between 1 and 20.")
+def guess_number_game():
+    print("Hello! What is your name?")
+    name = input()
 
-num = random.randint(1, 20)
-numb = 0
-gss = 0
+    print(f"Well, {name}, I am thinking of a number between 1 and 20.")
+    number_to_guess = random.randint(1, 20)
+    guesses_taken = 0
 
-while numb != num:
-        numb = int(input("Take a guess.\n"))
-        gss += 1
-        if numb < num:
+    while True:
+        print("Take a guess.")
+        guess = int(input())
+        guesses_taken += 1
+
+        if guess < number_to_guess:
             print("Your guess is too low.")
-        elif numb > num:
+        elif guess > number_to_guess:
             print("Your guess is too high.")
-
-
-print(f"Good job, {name}! You guessed my number in {gss} guesses!")
+        else:
+            print(f"Good job, {name}! You guessed my number in {guesses_taken} guesses!")
+            break
---
+++ lab3/function1/14.py
@@ -0,0 +1,3 @@
+from guess_number import guess_number_game
+if __name__ == "__main__":
+    guess_number_game()
