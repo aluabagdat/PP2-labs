1b7518ea4b96a5944a102c4a460b36d86aebde73
alukabagdat <alukabagdat@gmail.com>
2025-02-17 09:28:20 +0500

    upd 1.py

--- lab4/dates/generates/5.py
+++
@@ -1,8 +0,0 @@
-"""Implement a generator that returns all numbers from (n) down to 0."""
-
-def fromnto0():
-    n = int(input())
-    decr = (int(i) for i in range (n, 0, -1))
-    for i in range(n):
-        print(next(decr))
-fromnto0()
