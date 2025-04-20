1e4c12de11aef6c1ef99a93b841c6c93492fe662
alukabagdat <alukabagdat@gmail.com>
2025-04-20 22:43:01 +0500

    lab11

--- lab11/1.py
+++ lab11/1.py
@@ -1,135 +1,144 @@
-import pygame 
-import sys 
-import random 
-import psycopg2 
- 
-pygame.init() 
- 
-SCREEN_WIDTH, SCREEN_HEIGHT = 600, 400 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
- 
-BLACK = (0, 0, 0) 
-WHITE = (255, 255, 255) 
-GREEN = (0, 255, 0) 
-RED = (255, 0, 0) 
- 
-snake_pos = [[100, 50], [90, 50], [80, 50]] 
-snake_speed = [10, 0] 
-food = {'pos': [0, 0], 'weight': 1, 'spawn_time': 0} 
-food_spawn = True 
-score = 0 
-level = 1 
-speed_increase = 0.1 
-food_counter = 0   
- 
-fps = pygame.time.Clock() 
-paused = False 
+import psycopg2 as pgsql
 
-def insert_score(name, score, level): 
-    conn = psycopg2.connect(dbname='lab10', user='postgres', password='Almaty250505', host='localhost', port='5433') 
-    cur = conn.cursor() 
-    insert_query = "INSERT INTO snake_game_scores (player_name, score, level) VALUES (%s, %s, %s)" 
-    cur.execute(insert_query, (name, score, level)) 
-    conn.commit() 
-    cur.close() 
-    conn.close() 
- 
-def get_scores(name): 
-    conn = psycopg2.connect(dbname='lab10', user='postgres', password='Almaty250505', host='localhost', port='5433') 
-    cur = conn.cursor() 
-    query = "SELECT score, level FROM snake_game_scores WHERE player_name = %s ORDER BY score DESC" 
-    cur.execute(query, (name,)) 
-    results = cur.fetchall() 
-    cur.close() 
-    conn.close() 
-    return results 
- 
-player_name = input("Enter your name: ") 
-player_name = player_name.encode('utf-8', 'ignore').decode('utf-8') 
-scores = get_scores(player_name) 
-if scores: 
-    print("Your previous scores:") 
-    for score, level in scores: 
-        print(f"Score: {score}, Level: {level}") 
-    sys.exit()   
- 
-def check_collision(pos): 
-    if pos[0] < 0 or pos[0] > SCREEN_WIDTH - 10 or pos[1] < 0 or pos[1] > SCREEN_HEIGHT - 10: 
-        return True 
-    if pos in snake_pos[1:]: 
-        return True 
-    return False 
- 
-def get_random_food(): 
-    global food_counter 
-    while True: 
-        pos = [random.randrange(1, (SCREEN_WIDTH // 10)) * 10, random.randrange(1, (SCREEN_HEIGHT // 10)) * 10] 
-        if pos not in snake_pos: 
-            weight = 2 if food_counter >= 2 else 1 
-            food_counter = 0 if weight == 2 else food_counter + 1 
-            return {'pos': pos, 'weight': weight, 'spawn_time': pygame.time.get_ticks()} 
- 
-try: 
-    while True: 
-        for event in pygame.event.get(): 
-            if event.type == pygame.QUIT: 
-                pygame.quit() 
-                sys.exit() 
-            elif event.type == pygame.KEYDOWN: 
-                if event.key == pygame.K_UP and snake_speed[1] == 0: 
-                    snake_speed = [0, -10] 
-                elif event.key == pygame.K_DOWN and snake_speed[1] == 0: 
-                    snake_speed = [0, 10] 
-                elif event.key == pygame.K_LEFT and snake_speed[0] == 0: 
-                    snake_speed = [-10, 0] 
-                elif event.key == pygame.K_RIGHT and snake_speed[0] == 0: 
-                    snake_speed = [10, 0] 
-                elif event.key == pygame.K_p: 
-                    paused = not paused 
- 
- 
-        if not paused: 
-            snake_pos.insert(0, list(map(lambda x, y: x + y, snake_pos[0], snake_speed))) 
- 
-            if check_collision(snake_pos[0]): 
-                insert_score(player_name, score, level) 
-                pygame.quit() 
-                sys.exit() 
- 
-            if snake_pos[0] == food['pos']: 
-                score += food['weight'] 
-                if score % 3 == 0: 
-                    level += 1 
-                    fps.tick(10 + level * speed_increase) 
-                food_spawn = True 
-            else: 
-                snake_pos.pop() 
- 
-            if food_spawn: 
-                food = get_random_food() 
-                food_spawn = False 
- 
-            current_time = pygame.time.get_ticks() 
-            if current_time - food['spawn_time'] > 10000: 
-                food_spawn = True 
- 
-        screen.fill(BLACK) 
-        for pos in snake_pos: 
-            pygame.draw.rect(screen, GREEN, pygame.Rect(pos[0], pos[1], 10, 10)) 
- 
-        food_color = RED if food['weight'] == 1 else (255, 165, 0) 
-        pygame.draw.rect(screen, food_color,
-        pygame.Rect(food['pos'][0], food['pos'][1], 10, 10)) 
- 
-        font = pygame.font.SysFont('arial', 20) 
-        score_text = font.render(f"Score: {score} Level: {level}", True, WHITE) 
-        screen.blit(score_text, [0, 0]) 
- 
-        if paused: 
-            pause_text = font.render("Paused", True, WHITE) 
-            screen.blit(pause_text, [SCREEN_WIDTH // 2 - 30, SCREEN_HEIGHT // 2]) 
- 
-        pygame.display.flip() 
-        fps.tick(10 + level * speed_increase) 
-except SystemExit: 
-    pygame.quit()
+connection=pgsql.connect(host="localhost", dbname="postgres", user="postgres", 
+                         password="AlekinTodorokiss-100", port=5434)
+cur=connection.cursor()
+
+
+def createpattern():
+    global query
+    query="""SELECT * FROM PhoneBook
+    WHERE """
+    print(r"Do you want to search by ID(0)/name(1)/break(any num) enter the number:")
+    mode=int(input())
+    if mode==0:
+            query+="ids"
+            print("Enter string")
+            substr=input()
+            print("""Select option:
+            1-ID is equal to string
+            2-ID starts with the string
+            3-ID ends with the string
+            4-ID contains the string""")
+            mode1=int(input())
+            if mode1==1:
+                query+="='{}'".format(substr)
+            elif mode1==2:
+                query+=" iLIKE '{}%'".format(substr)
+            elif mode1==3:
+                query+=" iLIKE '%{}'".format(substr)
+            else:
+                query+=" iLIKE '%{}%'".format(substr)
+    elif mode==2:
+            query+="""number"""
+            print("Enter string")
+            substr=input()
+            print("""Select option:
+            1-n is equal to string
+            2-n starts with the string
+            3-n ends with the string
+            4-n contains the string""")
+            mode1=int(input())
+            if mode1==1:
+                query+="='{}'".format(substr)
+            elif mode1==2:
+                query+=" iLIKE '{}%'".format(substr)
+            elif mode1==3:
+                query+=" iLIKE '%{}'".format(substr)
+            else:
+                query+=" iLIKE '%{}%'".format(substr)
+    else:
+         return "error"
+    return query
+
+
+def insert(surname, name, phone):
+    cur.execute("SELECT count(*) FROM PhoneBook WHERE surname='{}' AND name='{}'".format(ids, name))
+    if cur.fetchone()[0]==0:
+         cur.execute("""INSERT INTO PhoneBook VALUES ('{}','{}', {})""".format("ids", name, number))
+    else:
+         cur.execute("""UPDATE PhoneBook
+         SET number={}
+         WHERE ids='{}' AND name='{}'
+         """.format(number,ids, name))
+
+def loopinsert():
+    banned=[]
+    while True:
+        print("Want to enter a person's data? yes/no")
+        mode=input()
+        if mode=="no":
+             break
+        person=input().split()
+        if len(person)>3:
+             banned.append(person)
+             continue
+        
+        if not person[2].isdigit():
+             banned.append(person)
+             continue
+        
+        insert(*person)
+    if len(banned)==0:
+         return
+    print("This data were not added due to incorrect format:")
+    for i in banned:
+         print(i)
+        
+
+def pagination():
+    query=createpattern()
+    if query=="error":
+        return "error"
+    print("Need offset? yes/no:")
+    mode=input()
+    if mode=="yes":
+         print("Enter offset:")
+         offset=int(input())
+         query+=" OFFSET {}".format(offset)
+    print("Need limit? yes/no:")
+    mode=input()
+    if mode=="yes":
+         print("Enter limit:")
+         limit=int(input())
+         query+=" LIMIT {}".format(limit)
+    query +=";"
+    return query
+
+def delete():
+    query="""DELETE FROM PhoneBook
+    WHERE """
+    cur.execute("SELECT * from PhoneBook")
+    print(cur.fetchall())
+    print("Do you wanna delete by ID(0)/name(1)/number(2) enter the number")
+    mode=int(input())
+    if mode==0:
+         print("Enter ID to delete:")
+         surname=input()
+         query+="id='{}'".format(id)
+    elif mode==1:
+         print("Enter name to delete:")
+         name=input()
+         query+="name='{}'".format(name)
+    else:
+         print("Enter number to delete:")
+         number=input()
+         query+="number={}".format(number)
+    cur.execute(query)
+         
+
+s1=createpattern()
+if s1!="error":
+     cur.execute(s1+";")
+     print(cur.fetchall())
+#insert("Berik", "Serik", 12345)
+loopinsert()
+s1=pagination()
+if s1!="error":
+     cur.execute(s1+";")
+     print(cur.fetchall())
+delete()
+
+connection.commit()
+cur.close()
+connection.close()
